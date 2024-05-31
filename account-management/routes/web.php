9147d054dd152c0586de8b128b68fbcdc6ce82bc
Maelyn <maelyn.mei@gmail.com>
2024-06-01 02:39:42 +0800

    Account Management Auth

--- account-management/routes/web.php
+++ account-management/routes/web.php
@@ -3,8 +3,8 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->group(['prefix' => 'accounts'], function () use ($router) {
-    $router->get('/accounts', 'AccountController@listAccounts'); // List all accounts
-    $router->get('/accounts/{accountId}', 'AccountController@getAccount'); // Get specific account
-    $router->delete('/accounts/{accountId}', 'AccountController@deleteAccount'); // Delete specific account
-    $router->delete('/accounts', 'AccountController@deleteAllAccounts'); // Delete all accounts
+    $router->get('/', 'AccountController@listAccounts'); // List all accounts
+    $router->get('//{accountId}', 'AccountController@getAccount'); // Get specific account
+    $router->delete('//{accountId}', 'AccountController@deleteAccount'); // Delete specific account
+    $router->delete('/', 'AccountController@deleteAllAccounts'); // Delete all accounts
 });
